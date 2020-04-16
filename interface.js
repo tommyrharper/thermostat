653687068a909a9e94436b3ecd3c5e7476281e65
tommyrharper <tommyrharper@gmail.com>
2020-04-16 19:33:20 +0100

    style updated

--- interface.js
+++ interface.js
@@ -10,8 +10,8 @@ $(function() {
 
   var updateWeather = function(city) {
     $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
-      $('#london-weather').text(data.weather[0].description)
-      $('#london-temp').text(data.main.temp)
+      $('#london-weather').text("Weather: " + data.weather[0].description)
+      $('#london-temp').text("Temperature: " + data.main.temp + "°C")
     })
   }
 
@@ -20,31 +20,47 @@ $(function() {
     $("#energy-usage").html(thermostat.energyUsage())
     $("#temp").attr("class", thermostat.energyUsage())
   }
+
   var setPowerSavingMode = function(onOrOff) {
     $("#powersaving-mode").html(onOrOff)
   }
+
   $("#temp-increase").click(function() { 
     thermostat.increase(1);
     updateTemp()
   })
+
   $("#temp-decrease").click(function() { 
     thermostat.decrease(1);
     updateTemp()
   })
+
   $("#reset").click(function() { 
     thermostat.reset();
     updateTemp()
   })
+
   $("#powersaving-on").click(function() { 
     thermostat.powerSavingOn();
     updateTemp()
     setPowerSavingMode("On")
+    console.log(thermostat.savingOn)
   })
-  $("#powersaving-off").click(function() { 
-    thermostat.powerSavingOff();
-    updateTemp()
-    setPowerSavingMode("Off")
-  })
+
+$("#powersaving-off").click(function() { 
+if (thermostat.savingOn == true) {
+  thermostat.powerSavingOff();
+  updateTemp()
+  console.log(thermostat.savingOn)
+  $("#powersaving-off").text("Powersaving On")
+} else {
+  thermostat.powerSavingOn();
+  updateTemp()
+  console.log(thermostat.savingOn)
+  $("#powersaving-off").text("Powersaving Off")
+}
+})
+
   updateTemp()
   setPowerSavingMode("On")
   updateWeather()
